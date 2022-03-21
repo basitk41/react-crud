87803e4cd53c72f48537822d9054cbc46acd7ac9
Basit Ali <basitk41@gmail.com>
2022-03-21 12:51:40 +0500

    add user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React, { useState } from "react";
+import Form from "./components/form";
 const App = () => {
+  const formData = { name: "", age: "" };
+  const [users, setUsers] = useState([]);
+  const [form, setForm] = useState(formData);
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = () => {
+      form.id = users.length + 1;
+      setUsers([...users, form]);
+    setForm(formData);
+    // const data = [...users];
+    // data.push(form);
+    // setUsers(data);
+  };
   return (
     <div className="container">
       <div className="row">
+        <Form
+          form={form}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
       </div>
     </div>
   );
---
+++ src/components/form.js
@@ -0,0 +1,35 @@
+import React from "react";
+const Form = ({ form, handleChange, handleSubmit }) => {
+  return (
+    <div className="col-sm-6">
+      <h1 align="center">Form</h1>
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          type="text"
+          className="form-control"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Age</label>
+        <input
+          type="number"
+          className="form-control"
+          name="age"
+          value={form.age}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-success" onClick={handleSubmit}>
+          {"Add"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
